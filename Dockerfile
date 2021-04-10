ced67e16a153c1abbcad22cdbf5f9f3341271cee
Al johnson <al.johnson@target.com>
2021-04-10 13:57:49 -0500

    Switch to selenium

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,22 @@
 # set base image (host OS)
 FROM python:3.8
 
+# Adding trusting keys to apt for repositories
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+# Adding Google Chrome to the repositories
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+# Updating apt to see and install Google Chrome
+RUN apt-get -y update  && \
+    apt-get install -y google-chrome-stable \
+    -yqq unzip
+
+# Download the Chrome Driver
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+# Unzip the Chrome Driver into /usr/local/bin directory
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+# Set display port as an environment variable
+ENV DISPLAY=:99
+
 # set the working directory in the container
 WORKDIR /code
 
@@ -8,7 +24,7 @@ WORKDIR /code
 COPY src/requirements.txt .
 
 # install dependencies
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip && pip install -r requirements.txt
 
 # copy the content of the local src directory to the working directory
 COPY src/ .
--- src/app.py
+++ src/app.py
@@ -1,36 +1,21 @@
-import re
-import requests
-from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
 
-vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
-html_text = requests.get(vgm_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
-
-def download_track(count, track_element):
-    # Get the title of the track from the HTML element
-    track_title = track_element.text.strip().replace('/', '-')
-    download_url = '{}{}'.format(vgm_url, track_element['href'])
-    file_name = '{}_{}.mid'.format(count, track_title)
-
-    # Download the track
-    r = requests.get(download_url, allow_redirects=True)
-    with open(file_name, 'wb') as f:
-        f.write(r.content)
-
-    # Print to the console to keep track of how the scraping is coming along.
-    print('Downloaded: {}'.format(track_title, download_url))
-
-
-if __name__ == '__main__':
-    attrs = {
-        'href': re.compile(r'\.mid$')
-    }
-
-    tracks = soup.find_all('a', attrs=attrs, string=re.compile(r'^((?!\().)*$'))
-
-    count = 0
-    for track in tracks:
-        download_track(count, track)
-        count += 1
-    print(len(tracks))
+if __name__ == "__main__":
+    set_chrome_options()
+    driver = webdriver.Chrome(options=chrome_options)
+    # Do stuff with your driver
+    driver.close()
